c07a848f2d1696c7a53a7b7e613f701b072e196d
JonathanFlores444 <jonathan.flores289@ist17dejulio.edu.ec>
2023-11-13 19:46:42 -0500

    Validacion del campo nombre, evento onClick

--- app/src/main/java/com/jafa/holamundo/Formulario.java
+++ app/src/main/java/com/jafa/holamundo/Formulario.java
@@ -3,6 +3,10 @@ package com.jafa.holamundo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Formulario extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class Formulario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        //Crear objetos del layout
+        EditText edNombres,edApellidos,edCedula,edCorreo,edDireccion;
+        Button btValidar = findViewById(R.id.btnValidar);
+        edNombres=findViewById(R.id.txtNombres);
+        edApellidos=findViewById(R.id.txtApellidos);
+        edCedula=findViewById(R.id.txtCedula);
+        edCorreo=findViewById(R.id.txtCorreo);
+        edDireccion=findViewById(R.id.txtDireccion);
+
+        btValidar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre = edNombres.getText().toString();
+                if(!nombre.isEmpty()){
+                    Toast.makeText(Formulario.this,"Holi",Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
     }
 }
